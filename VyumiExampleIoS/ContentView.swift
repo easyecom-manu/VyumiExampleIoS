809e9bb3ab0f6e395026887d2b8ffefe68aa973f
Manu Mathew <manu@easyecom.io>
2026-06-22 15:38:27 +0530

    Initial Commit

---
+++ VyumiExampleIoS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VyumiExampleIoS
+//
+//  Created by Manu Mathew on 22/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VyumiExampleIoS/VyumiExampleIoSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VyumiExampleIoSApp.swift
+//  VyumiExampleIoS
+//
+//  Created by Manu Mathew on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct VyumiExampleIoSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
